87160b80c2c533096ce7d6c4e924d1124ce82903
AljosaVukicevic <aljosa.vukicevic85@gmail.com>
2019-06-23 21:28:30 +0200

    added datarepository

---
+++ ListaProizvoda/ListaProizvoda/Shared/DataRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace ListaProizvoda.Shared
+{
+    public abstract class DataRepository<C, T> : IDataRepository<T>
+        where T : class where C : DbContext, new()
+    {
+        private C context = new C();
+
+        public virtual C Context { get => context; set => context = value; }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            IQueryable<T> query = Context.Set<T>();
+            return query;
+        }
+
+        public virtual void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+        }
+
+        public virtual void Edit(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ ListaProizvoda/ListaProizvoda/Shared/IDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaProizvoda.Shared
+{
+    public interface IDataRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
+
+        void Add(T entity);
+        void Edit(T entity);
+    }
+}
