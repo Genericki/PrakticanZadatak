b8c19d254f5cea113fee61aa6e4fd50e9e6639ed
AljosaVukicevic <aljosa.vukicevic85@gmail.com>
2019-06-24 16:10:00 +0200

    adding Data Access

---
+++ ListaProizvoda/ListaProizvoda/DAL/ProizvodiRepository.cs
@@ -0,0 +1,35 @@
+﻿using ListaProizvoda.Models;
+using ListaProizvoda.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace ListaProizvoda.DAL
+{
+    public class ProizvodiRepository : DataRepository<ProizvodiContext, Proizvodi>
+    {
+        public override IQueryable<Proizvodi> GetAll()
+        {
+            return base.GetAll();
+        }
+
+        public override void Add(Proizvodi entity)
+        {
+            base.Add(entity);
+        }
+
+        public override void Edit(Proizvodi entity)
+        {
+            base.Edit(entity);
+        }
+
+        public Proizvodi FindBy(int? id)
+        {
+            var query = GetAll().FirstOrDefault(x => x.ProizvodID == id);
+            return query;
+        }
+
+    }
+}
