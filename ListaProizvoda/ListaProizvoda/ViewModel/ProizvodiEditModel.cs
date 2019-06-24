a0fbfcc5d1fec94c8c40acdaaf09593fdbfc16bf
AljosaVukicevic <aljosa.vukicevic85@gmail.com>
2019-06-24 16:14:53 +0200

    Added viewModel

---
+++ ListaProizvoda/ListaProizvoda/ViewModel/ProizvodiEditModel.cs
@@ -0,0 +1,28 @@
+﻿using ListaProizvoda.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ListaProizvoda.ViewModel
+{
+    public class ProizvodiEditModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Naziv { get; set; }
+
+        public string Opis { get; set; }
+
+        public string Kategorija { get; set; }
+
+        public string Proizvodjac { get; set; }
+
+        public string Dobavljac { get; set; }
+
+        public decimal? Cena { get; set; }
+
+    }
+}
---
+++ ListaProizvoda/ListaProizvoda/ViewModel/ProizvodiViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ListaProizvoda.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace ListaProizvoda.ViewModel
+{
+    public class ProizvodiViewModel
+    {
+       
+        public IQueryable<Proizvodi> DataProizvod { get; set; }
+
+        public string JsonStrings { get; set; }
+
+
+        #region Alias
+
+        [Required]
+        public int Id { get; set; }
+
+        public string Naziv { get; set; }
+
+        public string Opis { get; set; }
+
+        public string Kategorija { get; set; }
+
+        public string Proizvodjac { get; set; }
+
+        public string Dobavljac { get; set; }
+
+        public decimal? Cena { get; set; }
+
+        #endregion
+    }
+}
